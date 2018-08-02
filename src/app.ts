8784b223d6177a3228698c0b209b66000350c141
Elena Mischenko <emischenko@justcoded.com>
2018-08-02 17:32:15 +0300

    completed tasks 5 and 6

--- src/app.ts
+++ src/app.ts
@@ -49,7 +49,7 @@ function getAllBooks(): any[] {
   return allBooks;
 }
 
-function logFirstAvailable(books: any[]): void {
+function logFirstAvailable(books: any[] = getAllBooks()): void {
   const amount: number = books.length;
   let firstAvailableBook: any;
 
@@ -64,7 +64,7 @@ function logFirstAvailable(books: any[]): void {
   console.log(`First book name: ${firstAvailableBook.title}`);
 }
 
-function getBookTitlesByCategory(category: Category): Array<string> {
+function getBookTitlesByCategory(category: Category = Category.JavaScript): Array<string> {
   const allBooks = getAllBooks();
   let bookTitles: string[] = [];
 
@@ -91,21 +91,71 @@ function createCustomerID(name: string, id: number): string {
   return `${name}${id}`;
 }
 
-//Task 01
-logFirstAvailable(getAllBooks());
+//Task 05
+function createCustomer(name: string, age?: number, city?: string): void {
+  console.log(`Client name: ${name}`);
+  if (age) {
+    console.log(`Client age: ${age}`);
+  }
+  if (city) {
+    console.log(`Client city: ${city}`);
+  }
+}
 
-//Task 02
-const booksJSCategory: string[] = getBookTitlesByCategory(Category.JavaScript);
+function сheckoutBooks(customer: string, ...bookIDs: number[]): string[] {
+  console.log(`Checking books for customer ${customer}`);
+  let checkedBooks: string[] = [];
+  bookIDs.forEach(id => {
+    const book = getBookByID(id);
+    if (book.available) {
+      checkedBooks.push(book.title);
+    }
+  });
+  return checkedBooks;
+}
+
+//Task 06 
+function getTitles(author: string);
+function getTitles(available: boolean);
+function getTitles(parameter: string | boolean): string[] {
+  const allBooks = getAllBooks();
+  let bookTitles: string[] = [];
+  let bookProperty: string = '';
+
+  if (typeof parameter === 'string') {
+    bookProperty = 'author';
+  }
+  if (typeof parameter === 'boolean') {
+    bookProperty = 'available';
+  }
+
+  allBooks.forEach(book => {
+    if (book[bookProperty] === parameter) {
+      bookTitles.push(book.title);
+    }
+  });
+
+  return bookTitles;
+}
+
+
+//Task 05
+createCustomer('Ann');
+createCustomer('Mary', 28);
+createCustomer('Nick', 34, 'London');
+
+const booksJSCategory: string[] = getBookTitlesByCategory();
 logBookTitles(booksJSCategory);
 
-//Task 03 
-booksJSCategory.forEach((title, i) => console.log(`Book #${++i} via forEach: ${title}`));
-console.log(getBookByID(1));
-console.log(getBookByID(4));
+logFirstAvailable();
 
-//Task 04
-const myID = createCustomerID('Ann', 10);
-console.log(myID);
-let idGenerator: (s1: string, s2: number) => string;
-idGenerator = createCustomerID;
-console.log(idGenerator('Max', 4));
+let myBooks = сheckoutBooks('Ann', 1, 2, 4);
+myBooks.forEach(book => console.log(`Available book: ${book}`));
+
+//Task 06
+let checkedOutBooks = getTitles(true);
+checkedOutBooks.forEach(book => console.log(`Books, checked by available=true: ${book}`));
+console.log('__________________________________________________________________________');
+checkedOutBooks = getTitles('Evan Burchard');
+checkedOutBooks.forEach(book => console.log(`Books, checked by author Evan Burchard: ${book}`));
+
